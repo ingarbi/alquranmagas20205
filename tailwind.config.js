fc4f878656505e1d45e04fade3ecf49cd6869570
ingarbi <104501854+ingarbi@users.noreply.github.com>
2025-06-20 20:37:53 +0300

    small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
-  },
+       extend: {
+         fontFamily: {
+           'intro-book': ['Intro Book', 'sans-serif'],
+         },
+       },
+     },
   plugins: [],
 }
